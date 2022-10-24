082c6635a7a2b36f4296d36eea0f4fb625b1a0ea
Ana Luiza <analuiza0906@hotmail.com>
2022-10-24 17:22:11 -0300

    Req 4 - tela de login incompleta

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="meals">
+    /* <div className="meals">
       <span className="logo">TRYBE</span>
       <object
         className="rocksGlass"
         type="image/svg+xml"
         data={ rockGlass }
       >
-        Glass oi
+        Glass
       </object>
-    </div>
+    </div> */
+    <Login />
   );
 }
 
