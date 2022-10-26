a2dd087e314a70ac5fcb4101e1c8106b09ffbce2
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-10-25 23:59:41 -0300

    Requisito 10

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -1,10 +1,12 @@
 import Header from './Header';
+import SearchBar from './SearchBar';
 
 function Drinks() {
   return (
     <div>
       <Header titlePage="Drinks" />
       <h1> Drinks </h1>
+      <SearchBar />
     </div>
   );
 }
