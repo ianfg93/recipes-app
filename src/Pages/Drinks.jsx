77a5600eac555c0f9d5b8ccf2e4c5faa9d176aff
aires1294 <pedrotja.engenharia@gmail.com>
2022-10-26 14:58:25 -0300

    footer e 57/59 FEITOS

--- src/Pages/Drinks.jsx
+++ src/Pages/Drinks.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 
@@ -8,6 +9,7 @@ function Drinks() {
       Drinks
       <Header />
       <SearchBar />
+      <Footer />
     </div>
   );
 }
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Profile() {
   return (
     <div>
       Profile
       <Header />
+      <div>
+        <p data-testid="profile-email" />
+        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button type="button" data-testid="profile-logout-btn">Logout</button>
+      </div>
+      <Footer />
     </div>
   );
 }
