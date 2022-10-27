aff2dd286b2d538f2dfbf0a3a44a3f6716607e71
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-10-27 19:37:10 -0300

    Requisito 13

--- src/Pages/Drinks.jsx
+++ src/Pages/Drinks.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
+import myContext from '../context/myContext';
 
 function Drinks() {
+  const { ingredientApi } = useContext(myContext);
+
   return (
     <div>
-      Drinks
       <Header />
       <SearchBar />
+      <h1> Drinks </h1>
+      <div>
+        {
+          ingredientApi.length === 1 && (
+            <Redirect to={ `/drinks/${ingredientApi[0].idDrink}` } />
+          )
+        }
+      </div>
+
       <Footer />
     </div>
   );
--- src/Pages/Meals.jsx
+++ src/Pages/Meals.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import myContext from '../context/myContext';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import Footer from '../components/Footer';
 
 function Meals() {
+  const { ingredientApi } = useContext(myContext);
+
   return (
     <div>
-      Tela de receitas
       <Header />
       <SearchBar />
+      <h1> Tela de receitas </h1>
+      <div>
+        {
+          ingredientApi.length === 1 && (
+            <Redirect to={ `/meals/${ingredientApi[0].idMeal}` } />
+          )
+        }
+      </div>
       <Footer />
     </div>
   );
