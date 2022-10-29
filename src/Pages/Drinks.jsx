689dbc0888e7a7eceecc0e9afb4135128a19d825
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-10-29 01:39:32 -0300

    esqueci de commitar, então virou commit bomba: req 19,20,21,22 e 23(está faltando testes dos req 20 e 23)

--- src/Pages/Drinks.jsx
+++ src/Pages/Drinks.jsx
@@ -1,13 +1,47 @@
-import React, { useContext } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Redirect, Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import myContext from '../context/myContext';
+import Recipes from './Recipes';
+
+const MAX_RECIPES = 12;
 
 function Drinks() {
-  const { ingredientApi } = useContext(myContext);
-  const MAX_RECIPES = 12;
+  const [toggle, setToggle] = useState(true);
+
+  const {
+    ingredientApi,
+    filterApiReturn,
+    categoryDrinks,
+    initialDrinks,
+    handleApiDrinks,
+  } = useContext(myContext);
+
+  const itemCategory = categoryDrinks.filter((element, index) => index < Number('5'));
+
+  const buttonFilter = async ({ target: { textContent } }) => {
+    setToggle(!toggle);
+    if (toggle) {
+      const endPoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${textContent}`;
+      const response = await fetch(endPoint);
+      const { drinks } = await response.json();
+      const listDrinks = drinks.filter((element, index) => index < MAX_RECIPES);
+      handleApiDrinks(listDrinks);
+    }
+    if (!toggle) {
+      handleApiDrinks(initialDrinks);
+    }
+  };
+
+  const clearFilter = async () => {
+    const endPoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+    const response = await fetch(endPoint);
+    const { drinks } = await response.json();
+    const listDrinks = drinks.filter((element, index) => index < MAX_RECIPES);
+    handleApiDrinks(listDrinks);
+  };
 
   return (
     <div>
@@ -16,30 +50,56 @@ function Drinks() {
       <h1> Drinks </h1>
       <div>
         {
-          ingredientApi.length === 1 && (
-            <Redirect to={ `/drinks/${ingredientApi[0].idDrink}` } />
-          )
+          itemCategory.map((element, index) => (
+            <button
+              type="button"
+              key={ index }
+              data-testid={ `${element.strCategory}-category-filter` }
+              onClick={ buttonFilter }
+            >
+              { element.strCategory }
+            </button>
+          ))
         }
-        {
-          ingredientApi.filter((element, index) => index < MAX_RECIPES)
-            .map((element, index) => (
-              <div
-                key={ index }
-                data-testid={ `${index}-recipe-card` }
-              >
-                <img
-                  src={ element.strDrinkThumb }
-                  alt="imagem"
-                  data-testid={ `${index}-card-img` }
-                />
-                <p
-                  data-testid={ `${index}-card-name` }
-                >
-                  {element.strDrink}
-                </p>
-              </div>))
-        }
+        <button
+          type="button"
+          data-testid="All-category-filter"
+          onClick={ clearFilter }
+        >
+          All
+        </button>
       </div>
+
+      {!filterApiReturn ? (<Recipes />) : (
+        <div>
+          {
+            ingredientApi.length === 1 && (
+              <Redirect to={ `/drinks/${ingredientApi[0].idDrink}` } />
+            )
+          }
+          {
+            ingredientApi.filter((element, index) => index < MAX_RECIPES)
+              .map((element, index) => (
+                <div
+                  key={ index }
+                  data-testid={ `${index}-recipe-card` }
+                >
+                  <Link to={ `/drinks/${element.idDrink}` }>
+                    <img
+                      src={ element.strDrinkThumb }
+                      alt="imagem"
+                      data-testid={ `${index}-card-img` }
+                    />
+                    <p
+                      data-testid={ `${index}-card-name` }
+                    >
+                      {element.strDrink}
+                    </p>
+                  </Link>
+                </div>))
+          }
+        </div>
+      )}
       <Footer />
     </div>
   );
--- src/Pages/Meals.jsx
+++ src/Pages/Meals.jsx
@@ -1,14 +1,48 @@
-import React, { useContext } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Redirect, Link } from 'react-router-dom';
 import myContext from '../context/myContext';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import Footer from '../components/Footer';
+import Recipes from './Recipes';
+
+const MAX_RECIPES = 12;
 
 function Meals() {
-  const { ingredientApi } = useContext(myContext);
+  const [toggle, setToggle] = useState(true);
 
-  const MAX_RECIPES = 12;
+  const {
+    ingredientApi,
+    filterApiReturn,
+    categoryMeals,
+    initialMeals,
+    handleApiMeals,
+  } = useContext(myContext);
+
+  const itemCategory = categoryMeals.filter((element, index) => index < Number('5'));
+
+  const buttonFilter = async ({ target: { textContent } }) => {
+    setToggle(!toggle);
+    if (toggle) {
+      const endPoint = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${textContent}`;
+      const response = await fetch(endPoint);
+      const { meals } = await response.json();
+      const listMeals = meals.filter((element, index) => index < MAX_RECIPES);
+      handleApiMeals(listMeals);
+    }
+    if (!toggle) {
+      handleApiMeals(initialMeals);
+    }
+  };
+
+  const clearFilter = async () => {
+    const endPoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+    const response = await fetch(endPoint);
+    const { meals } = await response.json();
+    const listMeals = meals.filter((element, index) => index < MAX_RECIPES);
+    handleApiMeals(listMeals);
+  };
+
   return (
     <div>
       <Header />
@@ -16,28 +50,54 @@ function Meals() {
       <h1> Tela de receitas </h1>
       <div>
         {
-          ingredientApi.length === 1 ? (
-            <Redirect to={ `/meals/${ingredientApi[0].idMeal}` } />
-          ) : (
-            ingredientApi.filter((element, index) => index < MAX_RECIPES)
-              .map((element, index) => (
-                <div
-                  key={ index }
-                  data-testid={ `${index}-recipe-card` }
-                >
-                  <img
-                    src={ element.strMealThumb }
-                    alt="imagem"
-                    data-testid={ `${index}-card-img` }
-                  />
-                  <p
-                    data-testid={ `${index}-card-name` }
-                  >
-                    {element.strMeal}
-                  </p>
-                </div>)))
+          itemCategory.map((element, index) => (
+            <button
+              type="button"
+              key={ index }
+              data-testid={ `${element.strCategory}-category-filter` }
+              onClick={ buttonFilter }
+            >
+              {element.strCategory}
+            </button>
+          ))
         }
+        <button
+          type="button"
+          data-testid="All-category-filter"
+          onClick={ clearFilter }
+        >
+          All
+        </button>
       </div>
+      { !filterApiReturn ? (<Recipes />) : (
+        <div>
+          {
+            ingredientApi.length === 1 ? (
+              <Redirect to={ `/meals/${ingredientApi[0].idMeal}` } />
+            ) : (
+              ingredientApi.filter((element, index) => index < MAX_RECIPES)
+                .map((element, index) => (
+                  <div
+                    key={ index }
+                    data-testid={ `${index}-recipe-card` }
+                  >
+                    <Link to={ `/meals/${element.idMeal}` }>
+                      <img
+                        src={ element.strMealThumb }
+                        alt="imagem"
+                        data-testid={ `${index}-card-img` }
+                      />
+                      <p
+                        data-testid={ `${index}-card-name` }
+                      >
+                        {element.strMeal}
+                      </p>
+                    </Link>
+                  </div>)))
+          }
+        </div>
+      )}
+
       <Footer />
     </div>
   );
