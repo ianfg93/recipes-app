2ad34cc01da93e71d35d2afb6d24534f0a503fa1
Larismp <larissa.monteiro.urca@gmail.com>
2022-11-03 00:26:54 -0300

    requisito 25 incompleto e 26 done.

--- src/components/DrinkDetails.jsx
+++ src/components/DrinkDetails.jsx
@@ -1,25 +1,40 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import myContext from '../context/myContext';
 
 export default function DrinkDetails() {
 //   const { apiMealsDetails, setApiMealsDetails } = useContext(myContext);
-  const { setApiRecipeDetails } = useContext(myContext);
+  const { apiRecipeDetails, setApiRecipeDetails } = useContext(myContext);
   const paramsUrl = useParams();
-  console.log(paramsUrl.id);
+  const [drinksIngredients, measures] = useState([]);
 
   useEffect(() => {
     const setApi = async () => {
       const responseApi = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${paramsUrl.id}`);
       console.log(responseApi);
       const { drinks } = await responseApi.json();
-      setApiRecipeDetails(drinks[0]);
+      setApiRecipeDetails([drinks[0]]);
     };
     setApi();
-  }, [paramsUrl.id, setApiRecipeDetails]);
+  }, []);
 
   return (
-    <div>DrinkDetails</div>
+    <div>
+      DrinkDetails
+      <img
+        data-testid="recipe-photo"
+        src={ apiRecipeDetails.strDrinkThumb }
+        alt={ apiRecipeDetails.strDrink }
+      />
+      <h1 data-testid="recipe-title">{apiRecipeDetails.strDrink}</h1>
+      <h2 data-testid="recipe-category">{apiRecipeDetails.strCategory}</h2>
+      {apiRecipeDetails.map((ingredient, index) => (
+        <p key={ index } data-testid={ `${index}-ingredient-name-and-measure` }>
+          {(ingredient[index] !== null && measures[index] !== null)
+          || (ingredient[index].length !== 0 && measures[index].length !== 0)}
+        </p>))}
+      <p data-testid="instructions">{apiRecipeDetails.strInstructions}</p>
+    </div>
 
   );
 }
